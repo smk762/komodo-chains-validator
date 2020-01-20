f2451a20eb333ebf25efb61a5970d79decee11b2
smk762 <35845239+smk762@users.noreply.github.com>
2020-01-20 16:14:25 +0800

    Create chain_sync_looper.py

---
+++ chain_sync_looper.py
@@ -0,0 +1,21 @@
+import time
+from lib import validator_lib
+
+def main():
+    logger.warning("DONT RUN THIS ON YOUR NOTARY NODE!")
+    logger.warning("It will delete assetchain data folders after sync completes...")
+    for i in range(10):
+        time.sleep(1)
+        logger.warning("Use Ctl-C to exit... you have "+str(10-i)+" seconds...")
+    while True:
+        validator_lib.sim_chains_start_and_sync()
+
+if __name__ == '__main__':
+    import logging
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    main()
