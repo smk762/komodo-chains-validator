c914183605883be5e6e4ace31cf2b7b1c76d59dc
smk762 <35845239+smk762@users.noreply.github.com>
2020-01-20 17:28:39 +0800

    Update nn_report_hash.py

--- nn_report_hash.py
+++ nn_report_hash.py
@@ -1,6 +1,6 @@
 from lib import validator_lib
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 handler.setFormatter(formatter)
