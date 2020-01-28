a235988224ced6fb9b0f90e02dd3c36403ae232f
smk762 <smk762@iinet.net.au>
2020-01-28 02:27:37 -0500

    NN side

--- nn_report_hash.py
+++ nn_report_hash.py
@@ -1,10 +1,19 @@
 from lib import validator_lib
 import logging
+import sys
+
 logger = logging.getLogger()
-handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+fh = logging.FileHandler(sys.path[0]+'/nn_sync_report.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+handler = logging.StreamHandler()
 handler.setFormatter(formatter)
+
 logger.addHandler(handler)
+logger.addHandler(fh)
 logger.setLevel(logging.INFO)
 
 def main():
