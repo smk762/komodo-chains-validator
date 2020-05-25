a6de5220e51584cb8812a293af9e88911c112c07
smk762 <35845239+smk762@users.noreply.github.com>
2020-05-25 18:38:34 +0800

    Create electrum_lib.py

---
+++ alerts/electrum_lib.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import requests
+import socket
+import json
+import time
+import hashlib
+import codecs
+import base58
+import logging
+import logging.handlers
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+def get_from_electrum(url, port, method, params=[]):
+    try:
+        params = [params] if type(params) is not list else params
+        socket.setdefaulttimeout(5)
+        s = socket.create_connection((url, port))
+        s.send(json.dumps({"id": 0, "method": method, "params": params}).encode() + b'\n')
+        return json.loads(s.recv(99999)[:-1].decode())
+    except Exception as e:
+        print("==============================")
+        print(str(url)+""+str(port)+" failed!")
+        print(e)
+        print("==============================")
+
+def lil_endian(hex_str):
+    return ''.join([hex_str[i:i+2] for i in range(0, len(hex_str), 2)][::-1])
+
+def get_p2pk_scripthash_from_pubkey(pubkey):
+    scriptpubkey = '21' +pubkey+ 'ac'
+    scripthex = codecs.decode(scriptpubkey, 'hex')
+    s = hashlib.new('sha256', scripthex).digest()
+    sha256_scripthash = codecs.encode(s, 'hex').decode("utf-8")
+    script_hash = lil_endian(sha256_scripthash)
+    return script_hash
+
+def get_p2pkh_scripthash_from_pubkey(pubkey):
+    publickey = codecs.decode(pubkey, 'hex')
+    s = hashlib.new('sha256', publickey).digest()
+    r = hashlib.new('ripemd160', s).digest()
+    scriptpubkey = "76a914"+codecs.encode(r, 'hex').decode("utf-8")+"88ac"
+    h = codecs.decode(scriptpubkey, 'hex')
+    s = hashlib.new('sha256', h).digest()
+    sha256_scripthash = codecs.encode(s, 'hex').decode("utf-8")
+    script_hash = lil_endian(sha256_scripthash)
+    return script_hash
+
+def get_full_electrum_balance(pubkey, url, port):
+    p2pk_scripthash = get_p2pk_scripthash_from_pubkey(pubkey)
+    p2pkh_scripthash = get_p2pkh_scripthash_from_pubkey(pubkey)
+    p2pk_resp = get_from_electrum(url, port, 'blockchain.scripthash.get_balance', p2pk_scripthash)
+    p2pkh_resp = get_from_electrum(url, port, 'blockchain.scripthash.get_balance', p2pkh_scripthash)
+    p2pk_confirmed_balance = p2pk_resp['result']['confirmed']
+    p2pkh_confirmed_balance = p2pkh_resp['result']['confirmed']
+    p2pk_unconfirmed_balance = p2pk_resp['result']['unconfirmed']
+    p2pkh_unconfirmed_balance = p2pkh_resp['result']['unconfirmed']
+    total_confirmed = p2pk_confirmed_balance + p2pkh_confirmed_balance
+    total_unconfirmed = p2pk_unconfirmed_balance + p2pkh_unconfirmed_balance
+    total = total_confirmed + total_unconfirmed
+    return total
