44c8a5316dc01f0f6d944891cce77732f87de49f
smk762 <35845239+smk762@users.noreply.github.com>
2020-05-28 11:21:43 +0800

    Update electrum_lib.py

--- alerts/electrum_lib.py
+++ alerts/electrum_lib.py
@@ -21,12 +21,105 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
+class KMD_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 60,
+                       'SCRIPT_ADDR': 85,
+                       'SECRET_KEY': 188}
+
 class BTC_CoinParams(CoreMainParams):
     MESSAGE_START = b'\x24\xe9\x27\x64'
     DEFAULT_PORT = 7770
     BASE58_PREFIXES = {'PUBKEY_ADDR': 0,
                        'SCRIPT_ADDR': 5,
                        'SECRET_KEY': 128}
+
+class AYA_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 23,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 176}
+
+class EMC2_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 33,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 176}
+
+class GAME_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 38,
+                       'SCRIPT_ADDR': 5,
+                       'SECRET_KEY': 166}
+
+class GIN_CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 38,
+                       'SCRIPT_ADDR': 10,
+                       'SECRET_KEY': 198}
+
+# Update this if new third party coins added
+coin_params = {
+    "KMD": KMD_CoinParams,
+    "BTC": BTC_CoinParams,
+    "AYA": AYA_CoinParams,
+    "EMC2": EMC2_CoinParams,
+    "GAME": GAME_CoinParams,
+    "GIN": GIN_CoinParams,
+}
+
+# Get 3rd party / main dPoW coins
+all_coins = []
+main_coins = []
+third_party_coins = []
+
+r = requests.get("https://raw.githubusercontent.com/KomodoPlatform/dPoW/master/README.md")
+dpow_readme = r.text
+lines = dpow_readme.splitlines()
+for line in lines:
+    if line.find('dPoW-mainnet') != -1 or line.find('dPoW-3P') != -1:
+        raw_info = line.split("|")
+        info = [i.strip() for i in raw_info]
+        coin = info[0]
+        server = info[4]
+        all_coins.append(coin)
+        if server.lower() == 'dpow-mainnet':
+            main_coins.append(coin)
+        elif server.lower() == 'dpow-3p':
+            third_party_coins.append(coin)
+        else:
+            logger.info("UNRECOGNISED SERVER VALUE: "+server)
+
+main_coins = main_coins[:]+['BTC']
+third_party_coins = third_party_coins[:]+['KMD']
+antara_coins = main_coins[:]+['HUSH3', 'CHIPS', 'MCL']
+all_coins = third_party_coins[:]+main_coins
+
+main_coins.sort()
+third_party_coins.sort()
+antara_coins.sort()
+all_coins.sort()
+
+for coin in antara_coins:
+    coin_params.update({coin:KMD_CoinParams})
+
+electrums = {}
+r = requests.get('http://notary.earth:8762/api/info/coins/?dpow_active=1')
+coins_info = r.json()
+for coin in coins_info['results'][0]:
+    if len(coins_info['results'][0][coin]['electrums']) > 0:
+        electrum = coins_info['results'][0][coin]['electrums'][0].split(":") 
+        electrums.update({
+            coin:{
+                "url":electrum[0],
+                "port":electrum[1]
+                }
+            })
 
 def get_from_electrum(url, port, method, params=[]):
     try:
@@ -36,15 +129,16 @@ def get_from_electrum(url, port, method, params=[]):
         s.send(json.dumps({"id": 0, "method": method, "params": params}).encode() + b'\n')
         return json.loads(s.recv(99999)[:-1].decode())
     except Exception as e:
-        print("==============================")
-        print(str(url)+""+str(port)+" failed!")
-        print(e)
-        print("==============================")
+        logger.info("==============================")
+        logger.info(str(url)+""+str(port)+" failed!")
+        logger.info(e)
+        logger.info("==============================")
 
 def lil_endian(hex_str):
     return ''.join([hex_str[i:i+2] for i in range(0, len(hex_str), 2)][::-1])
 
-def get_addr_from_pubkey(pubkey):
+def get_addr_from_pubkey(pubkey, coin):
+    bitcoin.params = coin_params[coin]
     return str(P2PKHBitcoinAddress.from_pubkey(x(pubkey)))
 
 def get_p2pk_scripthash_from_pubkey(pubkey):
@@ -70,9 +164,9 @@ def get_full_electrum_balance(pubkey, url, port):
     p2pk_scripthash = get_p2pk_scripthash_from_pubkey(pubkey)
     p2pkh_scripthash = get_p2pkh_scripthash_from_pubkey(pubkey)
     p2pk_resp = get_from_electrum(url, port, 'blockchain.scripthash.get_balance', p2pk_scripthash)
-    print(p2pk_resp)
+    logger.info(p2pk_resp)
     p2pkh_resp = get_from_electrum(url, port, 'blockchain.scripthash.get_balance', p2pkh_scripthash)
-    print(p2pkh_resp)
+    logger.info(p2pkh_resp)
     p2pk_confirmed_balance = p2pk_resp['result']['confirmed']
     p2pkh_confirmed_balance = p2pkh_resp['result']['confirmed']
     p2pk_unconfirmed_balance = p2pk_resp['result']['unconfirmed']
@@ -80,4 +174,54 @@ def get_full_electrum_balance(pubkey, url, port):
     total_confirmed = p2pk_confirmed_balance + p2pkh_confirmed_balance
     total_unconfirmed = p2pk_unconfirmed_balance + p2pkh_unconfirmed_balance
     total = total_confirmed + total_unconfirmed
-    return total
+    return total/100000000
+
+def get_dexstats_balance(chain, addr):
+    url = 'http://'+chain.lower()+'.explorer.dexstats.info/insight-api-komodo/addr/'+addr
+    r = requests.get(url)
+    balance = r.json()['balance']
+    return balance
+
+def get_balance(chain, addr, pubkey):
+    balance = -1
+    try:
+        if chain in electrums:
+            try:
+                url = electrums[chain]["url"]
+                port = electrums[chain]["port"]
+                source = url+":"+str(port)
+                balance = get_full_electrum_balance(pubkey, url, port)
+            except Exception as e:
+                print(chain+" "+source+" ERR: "+str(e))
+                try:
+                    source = "dexstats"
+                    balance = get_dexstats_balance(chain, addr)
+                except Exception as e:
+                    print(chain+" "+source+" ERR: "+str(e))
+
+        elif chain in antara_coins:
+            try:
+                source = "dexstats"
+                balance = get_dexstats_balance(chain, addr)
+            except Exception as e:
+                print(chain+" "+source+" ERR: "+str(e))
+
+        elif chain == "GIN":
+            print("gin is dead?")
+            source = "None"
+        elif chain == "AYA":
+            url = 'https://explorer.aryacoin.io/ext/getaddress/'+addr
+            r = requests.get(url)
+            try:
+                source = 'explorer.aryacoin.io'
+                balance = r.json()['balance']
+            except Exception as e:
+                print(chain+" "+source+" ERR: "+str(e))
+
+    except Exception as e:
+        print("get_balance ERR: "+str(e))
+    if balance != -1:
+        print("<<<<< "+chain+" via ["+source+"] OK | addr: "+addr+" | balance: "+str(balance))
+    else:
+        print("##### "+chain+" via ["+source+"] FAILED | addr: "+addr+" | balance: "+str(balance))
+    return balance, source
